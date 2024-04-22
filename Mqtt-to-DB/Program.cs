ed75dc33f81570cc45280c5719b033f32b2b833d
maurobortolani <mauro.bortolani@ialedu.it>
2024-04-22 16:33:53 +0200

    insert sql

--- Mqtt-to-DB/Program.cs
+++ Mqtt-to-DB/Program.cs
@@ -1,9 +1,30 @@
-﻿using System.Data.SqlClient;
+﻿using Microsoft.SqlServer.Server;
+using System.Data.SqlClient;
 
 public class Program
 {
+	public class Dato
+	{
+		public int Id { get; set; }
+		public DateTime DateTime { get; set; }
+		public string Temperatura { get; set; }
+	}
 
 	static void Main()
+	{
+
+		Dato dato = new Dato()
+		{
+			Temperatura = (15.25).ToString()
+		};
+
+
+
+		scriviDato(dato);
+		
+	}	
+
+	public static void scriviDato(Dato x)
 	{
 		string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
 			"AttachDbFilename=C:\\Users\\bortolanim\\Desktop\\2IOT-ProgettoStage\\2iot-progettostage\\2IOTDB.mdf;" +
@@ -12,20 +33,52 @@ public class Program
 
 		try
 		{
-			using(SqlConnection conn = new SqlConnection(connectionString))
+			using (SqlConnection conn = new SqlConnection(connectionString))
+			{
+				conn.Open();
+				string queryInsert = $"INSERT INTO Dati (Message) VALUES ('{x.Temperatura}')";
+
+				using (SqlCommand cmd = new SqlCommand(queryInsert, conn))
+				{
+					cmd.ExecuteNonQuery();
+				}
+
+				conn.Close();
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.ToString());
+		}
+	}
+
+	public static void readDB()
+	{
+		string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+			"AttachDbFilename=C:\\Users\\bortolanim\\Desktop\\2IOT-ProgettoStage\\2iot-progettostage\\2IOTDB.mdf;" +
+			"Integrated Security=True;" +
+			"Connect Timeout=30;";
+
+		List<Dato> dati = new List<Dato>();
+
+		try
+		{
+			using (SqlConnection conn = new SqlConnection(connectionString))
 			{
 				conn.Open();
 				string query = "SELECT * FROM Dati";
 
-				using(SqlCommand cmd = new SqlCommand(query, conn))
+				using (SqlCommand cmd = new SqlCommand(query, conn))
 				{
-					using(SqlDataReader reader = cmd.ExecuteReader())
+					using (SqlDataReader reader = cmd.ExecuteReader())
 					{
-						while(reader.Read())
+						while (reader.Read())
 						{
-							Console.WriteLine(reader.GetInt32(0) + " - " +
-								reader.GetDateTime(1).ToString() + " - " +
-								reader.GetDecimal(2).ToString() );
+							Dato dato = new Dato();
+							dato.Id = reader.GetInt32(0);
+							dato.DateTime = reader.GetDateTime(1);
+							//dato.Temperatura = reader.GetDecimal(2);
+							dati.Add(dato);
 						}
 					}
 				}
@@ -36,6 +89,12 @@ public class Program
 		catch (Exception ex)
 		{
 			Console.WriteLine(ex.ToString());
+		}
+
+
+		foreach (Dato dato in dati)
+		{
+			Console.WriteLine($"{dato.Id} - {dato.DateTime} - {dato.Temperatura}°C");
 		}
 	}
 }
--- Mqtt-to-DB/obj/Debug/net8.0/Mqtt-to-DB.AssemblyInfo.cs
+++ Mqtt-to-DB/obj/Debug/net8.0/Mqtt-to-DB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mqtt-to-DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e319222ac747ce71cebd7bf07729e55cd2cf365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+447633d8ab70818b86bd1da745f53fa04f0b454d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mqtt-to-DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mqtt-to-DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
